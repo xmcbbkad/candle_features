1543d85800d08fdb120a2b841986429a1be27dca
xmcbbkad <188135446@qq.com>
2022-11-14 19:12:38 +0800

    update

--- make_features.py
+++ make_features.py
@@ -7,318 +7,246 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-FEATURE_LIST = [
-    't0_close',
-    't0_close_ratio_last',
-    't1_open',
-    't1_open_ratio_last',
-    't1_high',
-    't1_high_ratio_last',
-    't1_low',
-    't1_low_ratio_last',
-    't1_close',
-    't1_close_ratio_last',
-    't2_open',
-    't2_open_ratio_last',
-    't2_high',
-    't2_high_ratio_last',
-    't2_low',
-    't2_low_ratio_last',
-    't2_close',
-    't2_close_ratio_last',
-    't3_open',
-    't3_open_ratio_last',
-    't3_high',
-    't3_high_ratio_last',
-    't3_low',
-    't3_low_ratio_last',
-    't3_close',
-    't3_close_ratio_last',
-    't4_open',
-    't4_open_ratio_last',
-    't4_high',
-    't4_high_ratio_last',
-    't4_low',
-    't4_low_ratio_last',
-    't4_close',
-    't4_close_ratio_last',
-    't5_open',
-    't5_open_ratio_last',
-    't5_high',
-    't5_high_ratio_last',
-    't5_low',
-    't5_low_ratio_last',
-    't5_close',
-    't5_close_ratio_last',
-    't6_open',
-    't6_open_ratio_last',
-    't6_high',
-    't6_high_ratio_last',
-    't6_low',
-    't6_low_ratio_last',
-    't6_close',
-    't6_close_ratio_last',
-    't7_open',
-    't7_open_ratio_last',
-    't7_high',
-    't7_high_ratio_last',
-    't7_low',
-    't7_low_ratio_last',
-    't7_close',
-    't7_close_ratio_last',
-    't8_open',
-    't8_open_ratio_last',
-    't8_high',
-    't8_high_ratio_last',
-    't8_low',
-    't8_low_ratio_last',
-    't8_close',
-    't8_close_ratio_last',
-    't9_open',
-    't9_open_ratio_last',
-    't9_high',
-    't9_high_ratio_last',
-    't9_low',
-    't9_low_ratio_last',
-    't9_close',
-    't9_close_ratio_last',
-]
 
 def t0_close(df, row_id):
-    return df.loc[row_id]['close']
+    return round(df.loc[row_id]['close'], 4)
 
 def t0_close_ratio_last(df, row_id):
-    return df.loc[row_id]['close']/df.loc[row_id-1]['close']
+    return round(df.loc[row_id]['close']/df.loc[row_id-1]['close'], 4)
 
 def t1_open(df, row_id):
-    return df.loc[row_id-1]['open']
+    return round(df.loc[row_id-1]['open'], 4)
 
 def t1_open_ratio_last(df, row_id):
-    return df.loc[row_id-1]['open']/df.loc[row_id-2]['close']
+    return round(df.loc[row_id-1]['open']/df.loc[row_id-2]['close'], 4)
 
 def t1_high(df, row_id):
-    return df.loc[row_id-1]['high']
+    return round(df.loc[row_id-1]['high'], 4)
 
 def t1_high_ratio_last(df, row_id):
-    return df.loc[row_id-1]['high']/df.loc[row_id-2]['close']
+    return round(df.loc[row_id-1]['high']/df.loc[row_id-2]['close'], 4)
 
 def t1_low(df, row_id):
-    return df.loc[row_id-1]['low']
+    return round(df.loc[row_id-1]['low'], 4)
 
 def t1_low_ratio_last(df, row_id):
-    return df.loc[row_id-1]['low']/df.loc[row_id-2]['close']
+    return round(df.loc[row_id-1]['low']/df.loc[row_id-2]['close'], 4)
 
 def t1_close(df, row_id):
-    return df.loc[row_id-1]['close']
+    return round(df.loc[row_id-1]['close'], 4)
 
 def t1_close_ratio_last(df, row_id):
-    return df.loc[row_id-1]['close']/df.loc[row_id-2]['close']
+    return round(df.loc[row_id-1]['close']/df.loc[row_id-2]['close'], 4)
 
 def t2_open(df, row_id):
-    return df.loc[row_id-2]['open']
+    return round(df.loc[row_id-2]['open'], 4)
 
 def t2_open_ratio_last(df, row_id):
-    return df.loc[row_id-2]['open']/df.loc[row_id-3]['close']
+    return round(df.loc[row_id-2]['open']/df.loc[row_id-3]['close'], 4)
 
 def t2_high(df, row_id):
-    return df.loc[row_id-2]['high']
+    return round(df.loc[row_id-2]['high'], 4)
 
 def t2_high_ratio_last(df, row_id):
-    return df.loc[row_id-2]['high']/df.loc[row_id-3]['close']
+    return round(df.loc[row_id-2]['high']/df.loc[row_id-3]['close'], 4)
 
 def t2_low(df, row_id):
-    return df.loc[row_id-2]['low']
+    return round(df.loc[row_id-2]['low'], 4)
 
 def t2_low_ratio_last(df, row_id):
-    return df.loc[row_id-2]['low']/df.loc[row_id-3]['close']
+    return round(df.loc[row_id-2]['low']/df.loc[row_id-3]['close'], 4)
 
 def t2_close(df, row_id):
-    return df.loc[row_id-2]['close']
+    return round(df.loc[row_id-2]['close'], 4)
 
 def t2_close_ratio_last(df, row_id):
-    return df.loc[row_id-2]['close']/df.loc[row_id-3]['close']
+    return round(df.loc[row_id-2]['close']/df.loc[row_id-3]['close'], 4)
 
 def t3_open(df, row_id):
-    return df.loc[row_id-3]['open']
+    return round(df.loc[row_id-3]['open'], 4)
 
 def t3_open_ratio_last(df, row_id):
-    return df.loc[row_id-3]['open']/df.loc[row_id-4]['close']
+    return round(df.loc[row_id-3]['open']/df.loc[row_id-4]['close'], 4)
 
 def t3_high(df, row_id):
-    return df.loc[row_id-3]['high']
+    return round(df.loc[row_id-3]['high'], 4)
 
 def t3_high_ratio_last(df, row_id):
-    return df.loc[row_id-3]['high']/df.loc[row_id-4]['close']
+    return round(df.loc[row_id-3]['high']/df.loc[row_id-4]['close'], 4)
 
 def t3_low(df, row_id):
-    return df.loc[row_id-3]['low']
+    return round(df.loc[row_id-3]['low'], 4)
 
 def t3_low_ratio_last(df, row_id):
-    return df.loc[row_id-3]['low']/df.loc[row_id-4]['close']
+    return round(df.loc[row_id-3]['low']/df.loc[row_id-4]['close'], 4)
 
 def t3_close(df, row_id):
-    return df.loc[row_id-3]['close']
+    return round(df.loc[row_id-3]['close'], 4)
 
 def t3_close_ratio_last(df, row_id):
-    return df.loc[row_id-3]['close']/df.loc[row_id-4]['close']
+    return round(df.loc[row_id-3]['close']/df.loc[row_id-4]['close'], 4)
 
 def t4_open(df, row_id):
-    return df.loc[row_id-4]['open']
+    return round(df.loc[row_id-4]['open'], 4)
 
 def t4_open_ratio_last(df, row_id):
-    return df.loc[row_id-4]['open']/df.loc[row_id-5]['close']
+    return round(df.loc[row_id-4]['open']/df.loc[row_id-5]['close'], 4)
 
 def t4_high(df, row_id):
-    return df.loc[row_id-4]['high']
+    return round(df.loc[row_id-4]['high'], 4)
 
 def t4_high_ratio_last(df, row_id):
-    return df.loc[row_id-4]['high']/df.loc[row_id-5]['close']
+    return round(df.loc[row_id-4]['high']/df.loc[row_id-5]['close'], 4)
 
 def t4_low(df, row_id):
-    return df.loc[row_id-4]['low']
+    return round(df.loc[row_id-4]['low'], 4)
 
 def t4_low_ratio_last(df, row_id):
-    return df.loc[row_id-4]['low']/df.loc[row_id-5]['close']
+    return round(df.loc[row_id-4]['low']/df.loc[row_id-5]['close'], 4)
 
 def t4_close(df, row_id):
-    return df.loc[row_id-4]['close']
+    return round(df.loc[row_id-4]['close'], 4)
 
 def t4_close_ratio_last(df, row_id):
-    return df.loc[row_id-4]['close']/df.loc[row_id-5]['close']
+    return round(df.loc[row_id-4]['close']/df.loc[row_id-5]['close'], 4)
 
 def t5_open(df, row_id):
-    return df.loc[row_id-5]['open']
+    return round(df.loc[row_id-5]['open'], 4)
 
 def t5_open_ratio_last(df, row_id):
-    return df.loc[row_id-5]['open']/df.loc[row_id-6]['close']
+    return round(df.loc[row_id-5]['open']/df.loc[row_id-6]['close'], 4)
 
 def t5_high(df, row_id):
-    return df.loc[row_id-5]['high']
+    return round(df.loc[row_id-5]['high'], 4)
 
 def t5_high_ratio_last(df, row_id):
-    return df.loc[row_id-5]['high']/df.loc[row_id-6]['close']
+    return round(df.loc[row_id-5]['high']/df.loc[row_id-6]['close'], 4)
 
 def t5_low(df, row_id):
-    return df.loc[row_id-5]['low']
+    return round(df.loc[row_id-5]['low'], 4)
 
 def t5_low_ratio_last(df, row_id):
-    return df.loc[row_id-5]['low']/df.loc[row_id-6]['close']
+    return round(df.loc[row_id-5]['low']/df.loc[row_id-6]['close'], 4)
 
 def t5_close(df, row_id):
-    return df.loc[row_id-5]['close']
+    return round(df.loc[row_id-5]['close'], 4)
 
 def t5_close_ratio_last(df, row_id):
-    return df.loc[row_id-5]['close']/df.loc[row_id-6]['close']
+    return round(df.loc[row_id-5]['close']/df.loc[row_id-6]['close'], 4)
 
 def t6_open(df, row_id):
-    return df.loc[row_id-6]['open']
+    return round(df.loc[row_id-6]['open'], 4)
 
 def t6_open_ratio_last(df, row_id):
-    return df.loc[row_id-6]['open']/df.loc[row_id-7]['close']
+    return round(df.loc[row_id-6]['open']/df.loc[row_id-7]['close'], 4)
 
 def t6_high(df, row_id):
-    return df.loc[row_id-6]['high']
+    return round(df.loc[row_id-6]['high'], 4)
 
 def t6_high_ratio_last(df, row_id):
-    return df.loc[row_id-6]['high']/df.loc[row_id-7]['close']
+    return round(df.loc[row_id-6]['high']/df.loc[row_id-7]['close'], 4)
 
 def t6_low(df, row_id):
-    return df.loc[row_id-6]['low']
+    return round(df.loc[row_id-6]['low'], 4)
 
 def t6_low_ratio_last(df, row_id):
-    return df.loc[row_id-6]['low']/df.loc[row_id-7]['close']
+    return round(df.loc[row_id-6]['low']/df.loc[row_id-7]['close'], 4)
 
 def t6_close(df, row_id):
-    return df.loc[row_id-6]['close']
+    return round(df.loc[row_id-6]['close'], 4)
 
 def t6_close_ratio_last(df, row_id):
-    return df.loc[row_id-6]['close']/df.loc[row_id-7]['close']
+    return round(df.loc[row_id-6]['close']/df.loc[row_id-7]['close'], 4)
 
 def t7_open(df, row_id):
-    return df.loc[row_id-7]['open']
+    return round(df.loc[row_id-7]['open'], 4)
 
 def t7_open_ratio_last(df, row_id):
-    return df.loc[row_id-7]['open']/df.loc[row_id-8]['close']
+    return round(df.loc[row_id-7]['open']/df.loc[row_id-8]['close'], 4)
 
 def t7_high(df, row_id):
-    return df.loc[row_id-7]['high']
+    return round(df.loc[row_id-7]['high'], 4)
 
 def t7_high_ratio_last(df, row_id):
-    return df.loc[row_id-7]['high']/df.loc[row_id-8]['close']
+    return round(df.loc[row_id-7]['high']/df.loc[row_id-8]['close'], 4)
 
 def t7_low(df, row_id):
-    return df.loc[row_id-7]['low']
+    return round(df.loc[row_id-7]['low'], 4)
 
 def t7_low_ratio_last(df, row_id):
-    return df.loc[row_id-7]['low']/df.loc[row_id-8]['close']
+    return round(df.loc[row_id-7]['low']/df.loc[row_id-8]['close'], 4)
 
 def t7_close(df, row_id):
-    return df.loc[row_id-7]['close']
+    return round(df.loc[row_id-7]['close'], 4)
 
 def t7_close_ratio_last(df, row_id):
-    return df.loc[row_id-7]['close']/df.loc[row_id-8]['close']
+    return round(df.loc[row_id-7]['close']/df.loc[row_id-8]['close'], 4)
 
 def t8_open(df, row_id):
-    return df.loc[row_id-8]['open']
+    return round(df.loc[row_id-8]['open'], 4)
 
 def t8_open_ratio_last(df, row_id):
-    return df.loc[row_id-8]['open']/df.loc[row_id-9]['close']
+    return round(df.loc[row_id-8]['open']/df.loc[row_id-9]['close'], 4)
 
 def t8_high(df, row_id):
-    return df.loc[row_id-8]['high']
+    return round(df.loc[row_id-8]['high'], 4)
 
 def t8_high_ratio_last(df, row_id):
-    return df.loc[row_id-8]['high']/df.loc[row_id-9]['close']
+    return round(df.loc[row_id-8]['high']/df.loc[row_id-9]['close'], 4)
 
 def t8_low(df, row_id):
-    return df.loc[row_id-8]['low']
+    return round(df.loc[row_id-8]['low'], 4)
 
 def t8_low_ratio_last(df, row_id):
-    return df.loc[row_id-8]['low']/df.loc[row_id-9]['close']
+    return round(df.loc[row_id-8]['low']/df.loc[row_id-9]['close'], 4)
 
 def t8_close(df, row_id):
-    return df.loc[row_id-8]['close']
+    return round(df.loc[row_id-8]['close'], 4)
 
 def t8_close_ratio_last(df, row_id):
-    return df.loc[row_id-8]['close']/df.loc[row_id-9]['close']
+    return round(df.loc[row_id-8]['close']/df.loc[row_id-9]['close'], 4)
 
 def t9_open(df, row_id):
-    return df.loc[row_id-9]['open']
+    return round(df.loc[row_id-9]['open'], 4)
 
 def t9_open_ratio_last(df, row_id):
-    return df.loc[row_id-9]['open']/df.loc[row_id-10]['close']
+    return round(df.loc[row_id-9]['open']/df.loc[row_id-10]['close'], 4)
 
 def t9_high(df, row_id):
-    return df.loc[row_id-9]['high']
+    return round(df.loc[row_id-9]['high'], 4)
 
 def t9_high_ratio_last(df, row_id):
-    return df.loc[row_id-9]['high']/df.loc[row_id-10]['close']
+    return round(df.loc[row_id-9]['high']/df.loc[row_id-10]['close'], 4)
 
 def t9_low(df, row_id):
-    return df.loc[row_id-9]['low']
+    return round(df.loc[row_id-9]['low'], 4)
 
 def t9_low_ratio_last(df, row_id):
-    return df.loc[row_id-9]['low']/df.loc[row_id-10]['close']
+    return round(df.loc[row_id-9]['low']/df.loc[row_id-10]['close'], 4)
 
 def t9_close(df, row_id):
-    return df.loc[row_id-9]['close']
+    return round(df.loc[row_id-9]['close'], 4)
 
 def t9_close_ratio_last(df, row_id):
-    return df.loc[row_id-9]['close']/df.loc[row_id-10]['close']
+    return round(df.loc[row_id-9]['close']/df.loc[row_id-10]['close'], 4)
 
 
-
-
-
-
-
+def read_feature_list(feature_file='./features.txt', mode='use'):
+    feature_list = []
+    with open(feature_file, 'r') as f:
+        for line in f.readlines():  
+            line = line.strip()
+            feature, use = line.split(' ')
+            if (mode == 'use' and use == '1') or mode == 'all':
+                feature_list.append(feature)
+    return feature_list        
 
 def make_feature_file(input_file, output_file):
     df_5mins = pd.read_csv(input_file, sep='\t')
-    df_features = pd.DataFrame(data=None, columns=FEATURE_LIST)
+    df_features = pd.DataFrame(data=None, columns=read_feature_list(feature_file='./features.txt', mode='all'))
 
     for row_id in range(10, df_5mins.shape[0]):
-        features = [eval(func)(df_5mins, row_id)  for func in FEATURE_LIST ] 
+        features = [eval(func)(df_5mins, row_id)  for func in read_feature_list(feature_file='./features.txt', mode='all')] 
         df_features.loc[len(df_features)] = features
     df_features.to_csv(output_file, sep='\t')
 
